8c7185cb5abf7a2058833576f1a2ea0ed0b336d6
Nigel Belderbos <nigel.belderbos@outlook.com>
2025-03-17 16:14:08 +0100

    test

---
+++ CleanUpsApp/CleanUpsApp/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace CleanUpsApp;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
