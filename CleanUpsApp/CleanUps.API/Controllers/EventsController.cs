95818ab9bd42da7af50c384770cb1bbf4fc596fe
Kevin Waweru <kew001@edu.zealand.dk>
2025-03-22 13:11:17 +0100

    CleanUps.API.Controllers - Some exception handling for EventController http methods

--- CleanUpsApp/CleanUps.API/Controllers/EventsController.cs
+++ CleanUpsApp/CleanUps.API/Controllers/EventsController.cs
@@ -19,61 +19,134 @@ namespace CleanUps.API.Controllers
         // GET: api/Events
         [HttpGet]
         [ProducesResponseType(StatusCodes.Status200OK)]
-        //[ProducesResponseType(StatusCodes.Status204NoContent)]
+        [ProducesResponseType(StatusCodes.Status204NoContent)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
         public async Task<IActionResult> GetAsync()
         {
-            List<EventDTO> events = await _eventService.GetAllAsync();
+            try
+            {
+                List<EventDTO> events = await _eventService.GetAllAsync();
 
-            return Ok(events);
+                if (events.Count == 0)
+                {
+                    return NoContent();
+                }
+
+                return Ok(events);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, ex.Message);
+            }
         }
 
         // GET api/Events/{id}
         [HttpGet()]
         [Route("{id}")]
         [ProducesResponseType(StatusCodes.Status200OK)]
-        //[ProducesResponseType(StatusCodes.Status404NotFound)]
-        //[ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
         public async Task<IActionResult> GetAsync(int id)
         {
-            EventDTO eventToGet = await _eventService.GetByIdAsync(id);
+            try
+            {
+                EventDTO eventToGet = await _eventService.GetByIdAsync(id);
 
-            return Ok(eventToGet);
+                return Ok(eventToGet);
+            }
+            catch (ArgumentException argEx)
+            {
+                return NotFound(argEx.Message);
+            }
+            catch (KeyNotFoundException knfEx)
+            {
+                return NotFound(knfEx.Message);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, ex.Message);
+            }
         }
 
         // POST api/Events
         [HttpPost]
         [ProducesResponseType(StatusCodes.Status201Created)]
-        //[ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
         public async Task<IActionResult> PostAsync([FromBody] EventDTO eventToBeAdded)
         {
-            EventDTO addedEvent = await _eventService.CreateAsync(eventToBeAdded);
+            try
+            {
+                EventDTO addedEvent = await _eventService.CreateAsync(eventToBeAdded);
 
-            return Created("api/Events/" + addedEvent.EventId, addedEvent);
+                return Created("api/Events/" + addedEvent.EventId, addedEvent);
+            }
+            catch (ArgumentException argEx)
+            {
+                return BadRequest(argEx.Message);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, ex.Message);
+            }
+
         }
 
         // PUT api/Events/{id}
         [HttpPut]
         [Route("{id}")]
         [ProducesResponseType(StatusCodes.Status200OK)]
-        //[ProducesResponseType(StatusCodes.Status404NotFound)]
-        //[ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
         public async Task<IActionResult> PutAsync([FromBody] EventDTO eventToBeUpdated)
         {
+            try
+            {
                 EventDTO updatedEvent = await _eventService.UpdateAsync(eventToBeUpdated);
 
                 return Ok(updatedEvent);
+            }
+            catch (ArgumentException aEx)
+            {
+                return BadRequest(aEx.Message);
+            }
+            catch (KeyNotFoundException knfEx)
+            {
+                return NotFound(knfEx.Message);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, ex.Message);
+            }
         }
 
         // DELETE api/Events/{id}
         [HttpDelete]
         [Route("{id}")]
         [ProducesResponseType(StatusCodes.Status200OK)]
-        //[ProducesResponseType(StatusCodes.Status404NotFound)]
-        //[ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
         public async Task<IActionResult> DeleteAsync(int id)
         {
-            EventDTO eventToBeDeleted = await _eventService.DeleteAsync(id);
-            return Ok(eventToBeDeleted);
+            try
+            {
+                EventDTO eventToBeDeleted = await _eventService.DeleteAsync(id);
+                return Ok(eventToBeDeleted);
+            }
+            catch (ArgumentException argEx)
+            {
+                return NotFound(argEx.Message);
+            }
+            catch (KeyNotFoundException knfEx)
+            {
+                return NotFound(knfEx.Message);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, ex.Message);
+            }
         }
 
         //Filter http methods to be done...
