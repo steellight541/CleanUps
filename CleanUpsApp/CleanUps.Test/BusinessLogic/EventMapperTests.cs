12901aff01e70a1f6e4f6bb99a91c75518589f99
Kevin Waweru <kew001@edu.zealand.dk>
2025-03-23 21:35:01 +0100

    EventMapper tests

--- CleanUpsApp/CleanUps.Test/BusinessLogic/EventMapperTests.cs
+++ CleanUpsApp/CleanUps.Test/BusinessLogic/EventMapperTests.cs
@@ -1,64 +1,82 @@
 ﻿using CleanUps.BusinessDomain.Models;
-using CleanUps.BusinessLogic.Interfaces.PrivateAccess;
 using CleanUps.Shared.DTOs;
+using CleanUps.BusinessLogic.Interfaces.PrivateAccess;
+using CleanUps.BusinessLogic.Services.Mappers;
 
-[TestClass]
+[TestClass()]
 public class EventMapperTests
 {
     private IMapper<Event, EventDTO> _mapper;
 
-    [TestInitialize]
+    [TestInitialize()]
     public void Setup()
     {
+        _mapper = new EventMapper();
     }
 
-    [TestMethod]
-    public void ToEvent_ValidEventDTO_MapsStreetNameCorrectly() 
+    [TestMethod()]
+    public void ConvertToDTO_Maps_EventId()
     {
         // Arrange
-        EventDTO eventDto = new EventDTO
-        {
-            EventId = 1,
-            StreetName = "Main St",
-            City = "Anytown",
-            ZipCode = "12345",
-            Country = "USA",
-            Description = "Cleanup",
-            DateOfEvent = DateOnly.FromDateTime(DateTime.Today),
-            StartTime = TimeOnly.FromTimeSpan(TimeSpan.Zero),
-            EndTime = TimeOnly.FromTimeSpan(TimeSpan.FromHours(1)),
-            Status = "Planned"
-        };
-
+        var model = new Event { EventId = 1 };
         // Act
-        Event eventModel = _mapper.ConvertToModel(eventDto);
-
+        var dto = _mapper.ConvertToDTO(model);
         // Assert
-        Assert.AreEqual("Main St", eventModel.StreetName);
+        Assert.AreEqual(1, dto.EventId);
     }
 
-    [TestMethod]
-    public void ToEventDTO_ValidEvent_MapsEventIdCorrectly()
+    [TestMethod()]
+    public void ConvertToDTO_Maps_StreetName()
     {
         // Arrange
-        Event eventModel = new Event
-        {
-            EventId = 1,
-            StreetName = "Main St",
-            City = "Anytown",
-            ZipCode = "12345",
-            Country = "USA",
-            Description = "Cleanup",
-            DateOfEvent = DateOnly.FromDateTime(DateTime.Today),
-            StartTime = TimeOnly.FromTimeSpan(TimeSpan.Zero),
-            EndTime = TimeOnly.FromTimeSpan(TimeSpan.FromHours(1)),
-            Status = "Planned"
-        };
-
+        var model = new Event { StreetName = "Main St" };
         // Act
-        EventDTO eventDto = _mapper.ConvertToDTO(eventModel);
-
+        var dto = _mapper.ConvertToDTO(model);
         // Assert
-        Assert.AreEqual(1, eventDto.EventId);
+        Assert.AreEqual("Main St", dto.StreetName);
+    }
+
+    [TestMethod()]
+    public void ConvertToDTO_Maps_DateOfEvent()
+    {
+        // Arrange
+        var model = new Event { DateOfEvent = DateOnly.FromDateTime(DateTime.Today) };
+        // Act
+        var dto = _mapper.ConvertToDTO(model);
+        // Assert
+        Assert.AreEqual(model.DateOfEvent, dto.DateOfEvent);
+    }
+
+    [TestMethod()]
+    public void ConvertToModel_Maps_EventId()
+    {
+        // Arrange
+        var dto = new EventDTO { EventId = 1 };
+        // Act
+        var model = _mapper.ConvertToModel(dto);
+        // Assert
+        Assert.AreEqual(1, model.EventId);
+    }
+
+    [TestMethod()]
+    public void ConvertToDTOList_Returns_CorrectCount()
+    {
+        // Arrange
+        var models = new List<Event> { new Event { EventId = 1 }, new Event { EventId = 2 } };
+        // Act
+        var dtoList = _mapper.ConvertToDTOList(models);
+        // Assert
+        Assert.AreEqual(2, dtoList.Count);
+    }
+
+    [TestMethod()]
+    public void ConvertToModelList_Returns_CorrectCount()
+    {
+        // Arrange
+        var listOfDTOs = new List<EventDTO> { new EventDTO { EventId = 1 }, new EventDTO { EventId = 2 } };
+        // Act
+        var listOfModels = _mapper.ConvertToModelList(listOfDTOs);
+        // Assert
+        Assert.AreEqual(2, listOfModels.Count);
     }
 }
