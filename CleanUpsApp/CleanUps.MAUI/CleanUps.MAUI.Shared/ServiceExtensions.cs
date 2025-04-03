2548e4f780166cd9faae6184dc7fd99a0cc3dc3f
Kevin Waweru <kew001@edu.zealand.dk>
2025-04-04 01:08:16 +0200

    api published

--- CleanUpsApp/CleanUps.MAUI/CleanUps.MAUI.Shared/ServiceExtensions.cs
+++ CleanUpsApp/CleanUps.MAUI/CleanUps.MAUI.Shared/ServiceExtensions.cs
@@ -8,6 +8,7 @@ namespace CleanUps.MAUI.Shared
         public static void AddApiServices(this IServiceCollection services)
         {
             services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("https://cleanup-rest-azc5hnbgdca3hwa6.westeurope-01.azurewebsites.net") });
+
             services.AddScoped<EventApiService>();
             services.AddScoped<EventAttendanceApiService>();
             services.AddScoped<PhotoApiService>();
