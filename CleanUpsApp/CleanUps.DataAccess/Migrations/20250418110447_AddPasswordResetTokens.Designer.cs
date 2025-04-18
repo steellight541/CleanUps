9fd582cb04372741ba64274dd379c5852f1ea93b
Kevin Waweru <kew001@edu.zealand.dk>
2025-04-18 13:05:43 +0200

    Migration worked Database Updated

--- CleanUpsApp/CleanUps.DataAccess/Migrations/20250418110447_AddPasswordResetTokens.Designer.cs
+++ CleanUpsApp/CleanUps.DataAccess/Migrations/20250418110447_AddPasswordResetTokens.Designer.cs
@@ -4,6 +4,7 @@ using CleanUps.DataAccess.DatabaseHub;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace CleanUps.DataAccess.Migrations
 {
     [DbContext(typeof(CleanUpsContext))]
-    partial class CleanUpsContextModelSnapshot : ModelSnapshot
+    [Migration("20250418110447_AddPasswordResetTokens")]
+    partial class AddPasswordResetTokens
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
