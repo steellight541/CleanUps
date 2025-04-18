50db81104da4199c85d1f7fcb75923265396c634
Kevin Waweru <kew001@edu.zealand.dk>
2025-04-18 18:15:53 +0200

    Passwordreset page works

--- CleanUpsApp/CleanUps.MAUI/CleanUps.MAUI.Shared/Services/ServiceExtensions.cs
+++ CleanUpsApp/CleanUps.MAUI/CleanUps.MAUI.Shared/Services/ServiceExtensions.cs
@@ -23,7 +23,7 @@ namespace CleanUps.MAUI.Shared.Services
         {
             services.AddHttpClient("CleanupsApi", client =>
             {
-                client.BaseAddress = new Uri("https://localhost:7128/");
+                client.BaseAddress = new Uri("https://cleanups-api-enbrcrevatgmhke7.canadacentral-01.azurewebsites.net/");
             });
 
             // Add simplified session services
