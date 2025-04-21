e88cf1f4c04619f358b3982c0051e8f7db6d48e1
Gorm42 <161018775+Gorm42@users.noreply.github.com>
2025-04-21 12:00:50 +0200

    Made MSTests for ErrorHandling under Shared. Trying to commit again.

---
+++ CleanUpsApp/CleanUps.APIMSTest1/Controllers/EventAttendancesControllerTests.cs
@@ -0,0 +1,71 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using CleanUps.API.Controllers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CleanUps.BusinessLogic.Services.Interfaces;
+using Microsoft.Extensions.Logging;
+
+namespace CleanUps.API.Controllers.Tests
+{
+    [TestClass()]
+    public class EventAttendancesControllerTests
+    {
+        //Mock data? Integration testing using Postman?
+        //https://stackoverflow.com/questions/3459287/whats-the-difference-between-a-mock-stub
+
+
+        private EventAttendancesController eventAttendanceConTest;
+        private readonly IEventAttendanceService _eventAttendanceService;
+        private readonly ILogger<EventAttendancesController> _logger;
+
+        public EventAttendancesControllerTests()
+        {
+            eventAttendanceConTest = new EventAttendancesController(_eventAttendanceService, _logger);
+        }
+
+        [TestMethod()]
+        public void EventAttendancesControllerTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void GetAllAsyncTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void GetEventsForASingleUserAsyncTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void GetUsersForASingleEventAsyncTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void PostAsyncTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void PutAsyncTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void DeleteAsyncTest()
+        {
+            Assert.Fail();
+        }
+    }
+}
